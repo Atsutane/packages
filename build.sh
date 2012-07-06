27794c98181b0604ddc1f597612a1d347c540ab5
Thorsten Töpper <atsutane@freethoughts.de>
2012-07-06 21:30:05 +0200

    [build.sh]dirty script for quick builds of VCS based packages.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+TARGET_REPO_DIR=/srv/http/repo64
+
+install -d $TARGET_REPO_DIR
+
+foreach pkgdir ( *-{git,svn})
+
+cd $pkgdir
+makepkg -f
+repo-remove `ls $TARGET_REPO_DIR/*.db.tar.gz` $pkgdir
+rm `ls $TARGET_REPO_DIR/$pkgdir-*.pkg.tar.xz`
+mv `ls $pkgdir-*.pkg.tar.xz` $TARGET_REPO_DIR 
+repo-add `ls $TARGET_REPO_DIR/*.db.tar.gz` `ls $TARGET_REPO_DIR/$pkgdir-*.pkg.tar.xz`
+cd ..
+
+end
+
