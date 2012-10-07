ee7d2dbb1fc51b19a837e502c755b87bece06741
Thorsten Töpper <atsutane@freethoughts.de>
2012-10-07 22:14:50 +0200

    [build-utgard.sh]Make it possible to specify packages.

--- build-utgard.sh
+++ build-utgard.sh
@@ -14,38 +14,61 @@ TARGET_REPO_DIR=$COMPILE_DIR/results
 SRC_DIR=`pwd`
 SRC_DIR_NAME=`pwd | sed -r s-^/.+/--`
 
+# Build the given package
+build_pkg() {
+	local pkgdir="${1}"
+	local origin=`pwd`
 
-install -d $TARGET_REPO_DIR
-
-# Make sure there is no such directory
-rm -rf $COMPILE_DIR/$SRC_DIR_NAME > /dev/null 2>&1
-
-cp -r $SRC_DIR $COMPILE_DIR
-cd $COMPILE_DIR/$SRC_DIR_NAME
-
-foreach pkgdir ( *-{git,svn})
 	cd $pkgdir
-	
+
 	# get rid of debug builds
 	rm -rf *.pkg.tar.xz ./src/*-build ./pkg > /dev/null 2>&1
 
+	# build the package
 	makepkg -f
-	mv *.pkg.tar.xz $TARGET_REPO_DIR 
+	mv *.pkg.tar.xz "$TARGET_REPO_DIR"
 
+	# get rid of unnecessary files
 	rm -rf ./src/*-build ./pkg
-	
-	cd ..
-	
+
+	cd "$origin"
+}
+
+# Prepare the build environment
+prepare() {
+	install -d "$TARGET_REPO_DIR"
+
+	# Make sure there is no previous directory
+	rm -rf "$COMPILE_DIR/$SRC_DIR_NAME" > /dev/null 2>&1
+	cp -r "$SRC_DIR" "$COMPILE_DIR"
+	cd "$COMPILE_DIR/$SRC_DIR_NAME"
+}
+
+
+prepare
+
+# Have there been packages specified?
+pkgs_to_build=()
+if [ "$#" -eq 0 ]; then
+	pkgs_to_build=(*-{git,svn})
+else
+	pkgs_to_build=($@)
+fi
+
+# Build the packages
+foreach pkgdir ($pkgs_to_build)
+	build_pkg "$pkgdir"
+
 	# sync the current state back to the origin.
-	rsync -acv $pkgdir $SRC_DIR
-	
+	rsync -acv "$pkgdir" "$SRC_DIR"
+
 	# Get rid of what has been saved.
-	rm -rf $pkgdir
+	rm -rf "$pkgdir"
 
 	# Need some time to get the CPU temperature down
 	sleep $SLEEP_TIME
 end
 
 # clean up
-rm -rf $COMPILE_DIR/$SRC_DIR_NAME
+rm -rf "$COMPILE_DIR/$SRC_DIR_NAME"
 
